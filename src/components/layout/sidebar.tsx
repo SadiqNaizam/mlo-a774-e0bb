631ec84f35e785827699e7949d86df25a6b50d0d
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-21 16:32:27 +0530

    Updated code files: sidebar.tsx

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -4,17 +4,19 @@ import SidebarNav from '../Dashboard/SidebarNav';
 
 interface SidebarProps {
   className?: string;
+  isCollapsed: boolean;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ className }) => {
+const Sidebar: React.FC<SidebarProps> = ({ className, isCollapsed }) => {
   return (
     <aside 
       className={cn(
-        "w-64 h-screen sticky top-0 flex-shrink-0 border-r bg-background", 
+        "h-screen sticky top-0 flex-shrink-0 border-r bg-background transition-all duration-300 ease-in-out",
+        isCollapsed ? "w-20" : "w-64", 
         className
       )}
     >
-      <SidebarNav />
+      <SidebarNav isCollapsed={isCollapsed} />
     </aside>
   );
 };
